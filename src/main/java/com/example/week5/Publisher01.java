d39b7818e56cf0b15c000c08af85db93e82981ae
Athichart <63070181@kmitl.ac.th>
2022-09-20 01:17:49 +0700

    week5

---
+++ src/main/java/com/example/week5/Publisher01.java
@@ -0,0 +1,20 @@
+package com.example.week5;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Publisher01 {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @RequestMapping(value = "/pow2/{num}", method = RequestMethod.GET)
+    public String pow2(@PathVariable("num") int num){
+        System.out.println("Publisher In : " + num );
+        rabbitTemplate.convertAndSend("MyDirectExchange","mobile",num);
+        return "Success";
+    }
+}
---
+++ src/main/java/com/example/week5/Word.java
@@ -0,0 +1,8 @@
+package com.example.week5;
+
+import java.util.ArrayList;
+
+public class Word {
+    public ArrayList<String> badword;
+    public ArrayList<String> goodword;
+}
